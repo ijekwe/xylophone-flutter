2ce5cbcbf2f46a9071b4b77a55842cd0186bd6de
iheanyiekwe <contactiheanyi@gmail.com>
2022-08-21 23:03:39 -0500

    buildKey updates

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,33 @@ class XylophoneApp extends StatelessWidget {
   Expanded buildKey({Color color, int soundNumber}) {
     return Expanded(
       child: TextButton(
-        color: color,
+        child: color,
         onPressed: () {
           playSound(soundNumber);
         },
       ),
     );
   }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
